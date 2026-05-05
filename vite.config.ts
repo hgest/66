efd2067bf1a5cf536983cafb2503c935795a60da
hgest <118880936+hgest@users.noreply.github.com>
2026-05-05 10:55:02 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,12 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
-import { inspectAttr } from 'plugin-inspect-react-code'
+import path from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: './',
-  plugins: [inspectAttr(), react()],
-  server: {
-    port: 3000,
-  },
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+})
